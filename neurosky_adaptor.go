7cf5a8cf8aeabcf5c50f296f1fb82e989495ed0d
Adrian Zankich <adrian@hybridgroup.com>
2014-04-22 20:49:04 -0700

    Correctly connect to serial port

--- neurosky_adaptor.go
+++ neurosky_adaptor.go
@@ -2,15 +2,22 @@ package gobotNeurosky
 
 import (
 	"github.com/hybridgroup/gobot"
+	"github.com/tarm/goserial"
+	"io"
 )
 
 type NeuroskyAdaptor struct {
 	gobot.Adaptor
-	sp gobot.Port
+	sp io.ReadWriteCloser
 }
 
 func (me *NeuroskyAdaptor) Connect() bool {
-	me.sp = gobot.ConnectToSerial(me.Adaptor.Port, 57600)
+	c := &serial.Config{Name: me.Adaptor.Port, Baud: 57600}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		panic(err)
+	}
+	me.sp = s
 	me.Connected = true
 	return true
 }
